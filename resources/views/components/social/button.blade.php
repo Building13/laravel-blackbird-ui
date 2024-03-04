b2fc74a374546b1a9729cca1c0c7799052af7f6c
Nathan Gross <nathan@bldg13.com>
2024-03-04 17:25:44 -0500

    add group to anchor

--- resources/views/components/social/button.blade.php
+++ resources/views/components/social/button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $href }}" target="_blank">
+<a href="{{ $href }}" target="_blank" class="group">
     <div {{ $attributes->merge(['class' => ' flex h-8 w-8 items-center justify-center rounded-full text-base transition-all hover:scale-105']) }}>
         <span class="sr-only">{{ $title }}</span>
         {{ $slot }}
