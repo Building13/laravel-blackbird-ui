2a31268c47766d64bac664a6d9d083ba1ddef393
Nathan Gross <nathan@bldg13.com>
2024-03-04 17:29:03 -0500

    remove text color

--- resources/views/components/social/button.blade.php
+++ resources/views/components/social/button.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ $href }}" target="_blank" class="group">
-    <div {{ $attributes->merge(['class' => ' flex h-8 w-8 items-center justify-center rounded-full text-base transition-all group-hover:scale-105']) }}>
+    <div {{ $attributes->merge(['class' => 'flex h-8 w-8 items-center justify-center rounded-full transition-all group-hover:scale-105']) }}>
         <span class="sr-only">{{ $title }}</span>
         {{ $slot }}
     </div>
